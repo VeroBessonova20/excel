8f5bdc3dc22c0925e27e146fa28ebc6e962673a7
Бессонова Вероника Алексеевна <vbessonova@phoenixit.ru>
2021-01-27 13:46:30 +0300

    DONE: project config part two

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsPlugins = () => {
+  const plugins = [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      },
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName('css')
+    }),
+  ];
+
+  if (isDev) {
+    // eslint-disable-next-line no-undef
+    plugins.push(new ESLintPlugin(options))
+  }
+  return plugins
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    },
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    hot: isDev,
+    port: 3000
+  },
+  plugins: jsPlugins(),
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
